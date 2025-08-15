f3880dc8eafd253e7482f3a093d6b0b1de60ec1e
Majo <monterosmarijo2004@gmil.com>
2025-08-14 22:21:29 -0500

    Cambiando mensajes

--- func.py
+++ func.py
@@ -1,10 +1,10 @@
 ingreso_mensual = 500
 
 if ingreso_mensual > 10000:
-    print("Estas bien en cualquier parte del mundo")
+    print("Te puedes mantener tranquilamente en cualquier parte del mundo")
 
 elif ingreso_mensual > 1000:
-    print("Estas bien en Latinoamérica")
+    print("Puedes sobrevivir en Latinoamérica")
 
 else:
-    print("sos pobre, es momento de ahorrar")
+    print("Sos pobre, es momento de ahorrar")
