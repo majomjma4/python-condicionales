504bc6547d5cf2fbc512ace23a3a674959d19d19
Majo :33 <monterosmarijo2004@gmail.com>
2025-08-14 22:30:14 -0500

    Agregando código de contraseña hecho en Python

---
+++ if_else.py
@@ -0,0 +1,9 @@
+contraseña_almacenada = "mjma1221"
+contraseña_escrita = "Majoxd"
+
+if contraseña_almacenada == contraseña_escrita:
+    print("INICIANDO SESIÓN...")
+else:
+   print("CONTRASEÑA EQUIVOCADA, INTENTA OTRA VEZ")
+
+ 
